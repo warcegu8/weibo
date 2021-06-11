0faf3db7a187f0da6abe53916f55ea173c55df5b
warcegu8 <changsun19807@126.com>
2021-06-11 22:24:25 +0800

    自定义撰写按钮

--- weibo/weibo/Classes/Module/Main/MainViewController.swift
+++ weibo/weibo/Classes/Module/Main/MainViewController.swift
@@ -14,13 +14,31 @@ class MainViewController: UITabBarController {
         super.viewDidLoad()
 
         addChildViewControllers()
+        // 无处不在懒加载，此时，tabBarButton 还没有创建
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
         
+        // 此时tabBatButton 都已经创建
+        setUpComposeButton()
+    }
+    
+    // 撰写按钮位置
+    private func setUpComposeButton() {
+        let w = tabBar.bounds.width / CGFloat(viewControllers!.count)
+        let rect = CGRect(x: 0, y: 0, width: w, height: tabBar.bounds.height)
+        
+        // 偏移
+        composeButton.frame = CGRectOffset(rect, 2 * w, 0)
     }
 
     // 添加所有控制器
     private func addChildViewControllers() {
         addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
         addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
+        // 这一步很关键
+        addChildViewController(UIViewController())
         addChildViewController(DiscoverTableViewController(), title: "发现", imageName: "tabbar_discover")
         addChildViewController(MessageTableViewController(), title: "我", imageName: "tabbar_profile")
     }
@@ -42,5 +60,25 @@ class MainViewController: UITabBarController {
     }
     
 
+    // 懒加载控件
+    lazy private var composeButton: UIButton = {
+        // 自定义类型的按钮
+        let button = UIButton()
+        
+        button.setImage(UIImage(named: "tabbar_compose_icon_add"), forState: UIControlState.Normal)
+        button.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), forState: UIControlState.Highlighted)
+        button.setBackgroundImage(UIImage(named: "tabbar_compose_button"), forState: UIControlState.Normal)
+        button.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), forState: UIControlState.Highlighted)
+        
+        self.tabBar.addSubview(button)
+        button.addTarget(self, action: "clickComposedButton", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        return button
+    }()
+    
+    
+    func clickComposedButton() {
+        print("中间按钮点击了")
+    }
     
 }
